d838278b1a547bc7aa9b364eb0c1cc27e4fc803b
zengliugen <779424225@qq.com>
2020-02-18 14:45:30 +0800

    完善反射相关

--- CxRouRou/Reflection/CxAssembly.cs
+++ CxRouRou/Reflection/CxAssembly.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 
 using CxSolution.CxRouRou.Expands;
+
+using Throw = CxSolution.CxRouRou.Util.CxThrow;
 
 namespace CxSolution.CxRouRou.Reflection
 {
@@ -11,6 +14,21 @@ namespace CxSolution.CxRouRou.Reflection
     /// </summary>
     public class CxAssembly
     {
+        /// <summary>
+        /// 默认绑定标志
+        /// </summary>
+        public const BindingFlags DefaultBindingFlag = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly;
+        /// <summary>
+        /// 获取程序集(内存)
+        /// </summary>
+        /// <param name="assemblyName"></param>
+        /// <returns></returns>
+        public static Assembly GetAssembly(string assemblyName)
+        {
+            Throw.ArgumentNullOrEmptyException(string.IsNullOrEmpty(assemblyName), "assemblyName");
+            var assembly = Assembly.Load(assemblyName);
+            return assembly;
+        }
         /// <summary>
         /// 获取程序集信息
         /// </summary>
@@ -94,5 +112,196 @@ namespace CxSolution.CxRouRou.Reflection
             }
             return infoSb.ToString();
         }
+        /// <summary>
+        /// 获取程序集信息
+        /// </summary>
+        /// <param name="assemblyName"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
+        public static CxAssemblyInfo GetAssemblyInfo(string assemblyName, BindingFlags bindingFlags = DefaultBindingFlag)
+        {
+            var assembly = GetAssembly(assemblyName);
+            if (assembly == null) return null;
+            CxAssemblyInfo assemblyInfo = new CxAssemblyInfo
+            {
+                Name = assembly.FullName,
+            };
+            var types = assembly.GetTypes();
+            if (types == null) return assemblyInfo;
+            assemblyInfo.TypeInfoList = new List<CxTypeInfo>(types.Length);
+            foreach (var type in types)
+            {
+                var typeInfo = new CxTypeInfo
+                {
+                    Name = type.FullName,
+                };
+                var properties = type.GetProperties(bindingFlags);
+                if (properties != null)
+                {
+                    typeInfo.PropertyInfoList = new List<CxPropertyInfo>(properties.Length);
+                    foreach (var property in properties)
+                    {
+                        var propertyInfo = new CxPropertyInfo
+                        {
+                            Name = property.Name,
+                            TypeName = property.PropertyType.FullName,
+                        };
+                        typeInfo.PropertyInfoList.Add(propertyInfo);
+                    }
+                }
+                var fields = type.GetFields(bindingFlags);
+                if (fields != null)
+                {
+                    typeInfo.FieldInfoList = new List<CxFieldInfo>(fields.Length);
+                    foreach (var field in fields)
+                    {
+                        var fieldInfo = new CxFieldInfo
+                        {
+                            Name = field.Name,
+                            TypeName = field.FieldType.FullName,
+                        };
+                        typeInfo.FieldInfoList.Add(fieldInfo);
+                    }
+                }
+                var events = type.GetEvents(bindingFlags);
+                if (events != null)
+                {
+                    typeInfo.EventInfoList = new List<CxEventInfo>(events.Length);
+                    foreach (var _event in events)
+                    {
+                        var eventInfo = new CxEventInfo()
+                        {
+                            Name = _event.Name,
+                            TypeName = _event.EventHandlerType.FullName,
+                        };
+                    }
+                }
+                var methods = type.GetMethods();
+                if (methods != null)
+                {
+                    typeInfo.MethodInfoList = new List<CxMethodInfo>(methods.Length);
+                    foreach (var method in methods)
+                    {
+                        var methodInfo = new CxMethodInfo()
+                        {
+                            Name = method.Name,
+                            ReturnTypeName = method.ReturnType.FullName,
+                        };
+                        var parameters = method.GetParameters();
+                        if (parameters != null)
+                        {
+                            methodInfo.ParameterTypeNameList = new List<string>();
+                            foreach (var parameter in parameters)
+                            {
+                                methodInfo.ParameterTypeNameList.Add(parameter.ParameterType.FullName);
+                            }
+                        }
+                        typeInfo.MethodInfoList.Add(methodInfo);
+                    }
+                }
+                assemblyInfo.TypeInfoList.Add(typeInfo);
+            }
+            return assemblyInfo;
+        }
+    }
+    /// <summary>
+    /// 程序集信息
+    /// </summary>
+    public class CxAssemblyInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 程序集下的类型信息列表
+        /// </summary>
+        public List<CxTypeInfo> TypeInfoList;
+    }
+    /// <summary>
+    /// 类型信息
+    /// </summary>
+    public class CxTypeInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 类型下的属性信息列表
+        /// </summary>
+        public List<CxPropertyInfo> PropertyInfoList;
+        /// <summary>
+        /// 类型下的字段信息列表
+        /// </summary>
+        public List<CxFieldInfo> FieldInfoList;
+        /// <summary>
+        /// 类型下的事件信息列表
+        /// </summary>
+        public List<CxEventInfo> EventInfoList;
+        /// <summary>
+        /// 类型下的函数信息列表
+        /// </summary>
+        public List<CxMethodInfo> MethodInfoList;
+    }
+    /// <summary>
+    /// 属性信息
+    /// </summary>
+    public class CxPropertyInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 类型名称
+        /// </summary>
+        public string TypeName;
+    }
+    /// <summary>
+    /// 字段信息
+    /// </summary>
+    public class CxFieldInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 类型名称
+        /// </summary>
+        public string TypeName;
+    }
+    /// <summary>
+    /// 事件信息
+    /// </summary>
+    public class CxEventInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 类型名称
+        /// </summary>
+        public string TypeName;
+    }
+    /// <summary>
+    /// 函数信息
+    /// </summary>
+    public class CxMethodInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 参数类型名称列表
+        /// </summary>
+        public List<string> ParameterTypeNameList;
+        /// <summary>
+        /// 返回类型名称
+        /// </summary>
+        public string ReturnTypeName;
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,36 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
-
+using System.Runtime.InteropServices;
 using CxSolution.CxRouRou.Reflection;
-
+using Newtonsoft.Json;
 namespace CxSolution.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var assembly = Assembly.Load("CxReflectionTest");
-            Debug.WriteLine(assembly.FullName);
-            Console.WriteLine(assembly.FullName);
-            var p1 = assembly.GetType("CxSolution.CxReflectionTest.TestBase");
-            var p2 = p1.GetField("base_public_int");
-            var i1 = assembly.CreateInstance("CxSolution.CxReflectionTest.TestBase");
-            var p3 = p2.GetValue(i1);
-            Console.WriteLine(p3);
-            Debug.WriteLine("------------------");
-            CxReflectionTest.TestBase.int_static = 50;
-            p2.SetValue(i1, 50);
-            var p4 = p2.GetValue(i1);
-            Console.WriteLine(p4);
-
-            CxReflectionTest.TestBase.int_static = 20;
-
-            var v1 = CxReflection.GetFieldValue(null, "CxReflectionTest", "CxSolution.CxReflectionTest.TestBase", "int_static");
-            Console.WriteLine(v1);
-
-
-
+            var assemblyInfo = CxAssembly.GetAssemblyInfo("CxReflectionTest");
+            var assemblyJson = JsonConvert.SerializeObject(assemblyInfo);
+            Console.WriteLine(assemblyJson);
             Console.ReadKey();
         }
     }
